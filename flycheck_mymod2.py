2702869038a25e965d6f66c8e7ee800275e9fa01
Reuven Lerner <reuven@lerner.co.il>
2021-04-01 20:33:21 +0300

    gitautosync update 64

---
+++ flycheck_mymod2.py
@@ -0,0 +1,14 @@
+"""A fantastic module for learning about modules!
+
+
+"""
+
+
+person = {'first': 'Reuven', 'last': 'Lerner', 'shoesize': 46}
+
+
+def mean(list_of_numbers):
+    """This is the best function ever written!
+
+So great, it gets a two-line docstring."""
+    return sum(list_of_numbers) / len(list_of_numbers)
