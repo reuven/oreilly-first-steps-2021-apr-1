f0b49e501c927578e472e2d76c57a35999c6fe2f
Reuven Lerner <reuven@lerner.co.il>
2021-04-01 21:54:53 +0300

    gitautosync update 178

--- menustuff/__init__.py
+++ menustuff/__init__.py
@@ -1,1 +1,4 @@
+# from . import menu  # imports the "menu" module into the "menu" variable
+
+# this imports the "menu" function from "menustuff.menu" into the "menu" variable
 from menustuff.menu import menu
