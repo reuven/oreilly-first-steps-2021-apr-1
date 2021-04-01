a9f75f364b54449f000756d787728f594f4262bc
Reuven Lerner <reuven@lerner.co.il>
2021-04-01 20:32:09 +0300

    gitautosync update 59

--- mymod2.py
+++ mymod2.py
@@ -2,4 +2,7 @@ person = {'first': 'Reuven', 'last': 'Lerner', 'shoesize': 46}
 
 
 def mean(list_of_numbers):
+    """This is the best function ever written!
+
+So great, it gets a two-line docstring."""
     return sum(list_of_numbers) / len(list_of_numbers)
