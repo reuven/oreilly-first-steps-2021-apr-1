c9825f7f7b2ad5bd7737430a12912fb9925436eb
Reuven Lerner <reuven@lerner.co.il>
2021-04-01 21:40:54 +0300

    gitautosync update 162

--- mypackage/flycheck___init__.py
+++ mypackage/flycheck___init__.py
@@ -1,6 +1,8 @@
 # when we say "import mypackage", this file (__init__.py) runs
 # when it runs, it then imports mymoda and mymodb from the current directory
 
+# note that . ONLY WORKS in the context of __init__.py 
+
 from . import mymoda
 from . import mymodb
 
