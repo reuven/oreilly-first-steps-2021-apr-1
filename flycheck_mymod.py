11828433c767d23830a6908eb86d1c5bf10676b7
Reuven Lerner <reuven@lerner.co.il>
2021-04-01 21:05:18 +0300

    gitautosync update 104

---
+++ flycheck_mymod.py
@@ -0,0 +1,16 @@
+import random   # here, mymod is importing random
+
+print(f'Hello from mymod!')
+
+x = 100
+
+y = [10, 20, 30]
+
+z = {'a': 1, 'b': 2, 'c': 3}
+
+
+def hello(name):
+    return f'Hello, {name}!'
+
+
+print('Goodbye from mymod!')
