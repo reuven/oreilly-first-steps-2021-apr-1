9668523ec88bc9163ce4eb454e02888068a07043
Reuven Lerner <reuven@lerner.co.il>
2021-04-01 21:55:01 +0300

    gitautosync update 179

--- menustuff/flycheck___init__.py
+++
@@ -1,4 +0,0 @@
-# from . import menu  # imports the "menu" module into the "menu" variable
-
-# this imports the "menu" function from "menustuff.menu" into the "menu" variable
-from menustuff.menu import menu
