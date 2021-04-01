6f01a26a6ac1d66ba9e2c027ef4629e0e0685512
Reuven Lerner <reuven@lerner.co.il>
2021-04-01 20:39:22 +0300

    gitautosync update 72

---
+++ flycheck_mymod.py
@@ -0,0 +1,16 @@
+import 
+
+print('Hello from mymod!')
+
+x = 100
+
+y = [10, 20, 30]
+
+z = {'a': 1, 'b': 2, 'c': 3}
+
+
+def hello(name):
+    return f'Hello, {name}!'
+
+
+print('Goodbye from mymod!')
