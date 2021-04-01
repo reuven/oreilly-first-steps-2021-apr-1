29e35d1d9f178ee744e5970526eb9b57f95f1ee5
Reuven Lerner <reuven@lerner.co.il>
2021-04-01 21:24:44 +0300

    gitautosync update 129

---
+++ flycheck_menu.py
@@ -0,0 +1,13 @@
+def menu(*args):  # args is a tuple containing all passed arguments
+    while True:
+        choices = '/'.join(sorted(args))
+        user_choice = input(f'Enter your choice ({choices}): ')
+
+        if user_choice in args:
+            return user_choice
+
+        # f-string, evaluates stuff in {}
+        print(f'Hey! {user_choice} was not in args!')
+
+
+if __name__ ==         
